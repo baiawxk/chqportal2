ff40951fafe75986521011d25d77fdaaeb06ce3c
陈洪强 <baiawxk@qq.com>
2019-01-27 22:17:50 +0800

    update

--- src/renderer/pages/demo/page2/index.vue
+++ src/renderer/pages/demo/page2/index.vue
@@ -1,35 +1,76 @@
 <template>
-    <d2-container>
-        <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
-        </b-field>
-
-        <b-field label="Email"
-            type="is-danger"
-            message="This email is invalid">
-            <b-input type="email"
-                value="john@"
-                maxlength="30">
-            </b-input>
-        </b-field>
-
-        <b-field label="Username"
-            type="is-success"
-            message="This username is available">
-            <b-input value="johnsilver" maxlength="30"></b-input>
-        </b-field>
-
-        <b-field label="Password"
-            type="is-warning"
-            :message="['Password is too short', 'Password must have at least 8 characters']">
-            <b-input value="123" type="password" maxlength="30"></b-input>
-        </b-field>
-
-        <b-field label="Subject">
-            <b-select placeholder="Select a subject">
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-            </b-select>
-        </b-field>
-    </d2-container>
+  <d2-container>
+    <template slot="header">Todos</template>
+    <el-container>
+      <el-aside>
+        <el-row class="tac">
+          <el-col :span="24">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+            >
+              <el-menu-item-group>
+                <template slot="title">Collect</template>
+                <el-menu-item index="1">infobox</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group>
+                <template slot="title">Focus</template>
+                <el-menu-item index="2">Today</el-menu-item>
+                <el-menu-item index="3">Next</el-menu-item>
+                <el-menu-item index="4">Tomorrow</el-menu-item>
+                <el-menu-item index="5">Schedule</el-menu-item>
+                <el-menu-item index="6">Maybe</el-menu-item>
+                <el-menu-item index="7">Wait</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="9">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>Projects</span>
+                </template>
+                <el-menu-item index="9-1">选项1</el-menu-item>
+                <el-menu-item index="9-2">选项2</el-menu-item>
+              </el-submenu>
+              <el-submenu index="10">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>Targets</span>
+                </template>
+                <el-menu-item index="10-1">选项1</el-menu-item>
+                <el-menu-item index="10-2">选项2</el-menu-item>
+              </el-submenu>
+              <el-submenu index="11">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>Situation</span>
+                </template>
+                <el-menu-item index="11-1">选项1</el-menu-item>
+                <el-menu-item index="11-2">选项2</el-menu-item>
+              </el-submenu>
+              <el-submenu index="12">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>Filters</span>
+                </template>
+                <el-menu-item index="12-1">选项1</el-menu-item>
+                <el-menu-item index="12-2">选项2</el-menu-item>
+              </el-submenu>
+              <el-menu-item-group>
+                <template slot="title">Finished</template>
+                <el-menu-item index="13">Done</el-menu-item>
+                <el-menu-item index="14">Trash</el-menu-item>
+              </el-menu-item-group>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main>
+        <div>
+          <el-input></el-input>
+        </div>
+        <br>
+      </el-main>
+    </el-container>
+  </d2-container>
 </template>
